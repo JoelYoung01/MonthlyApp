5868c5647308488365b3594b130012b6cbca5eef
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2025-01-30 20:26:06 -0600

    Add env var validation to build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,23 @@ import vue from "@vitejs/plugin-vue";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 import AutoImport from "unplugin-auto-import/vite";
 
+function validateVars(requiredEnvVars: string[]) {
+  return {
+    name: "ValidateVars",
+    config() {
+      const missingVars = requiredEnvVars.filter((envVar) => !process.env[envVar]);
+
+      if (missingVars.length > 0) {
+        throw new Error(
+          `Required environment variables are missing:\n` +
+            missingVars.map((v) => `- ${v}`).join("\n") +
+            `\n\nPlease check your .env file and ensure all required variables are defined.`
+        );
+      }
+    }
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,6 +29,7 @@ export default defineConfig({
       template: { transformAssetUrls }
     }),
     vuetify(),
+    validateVars(["VITE_APP_TITLE", "VITE_API_URL", "VITE_GOOGLE_CLIENT_ID"]),
     AutoImport({
       // https://github.com/unplugin/unplugin-auto-import?tab=readme-ov-file#configuration
       include: [/\.vue$/, /\.vue\?vue/, /\.ts$/],
